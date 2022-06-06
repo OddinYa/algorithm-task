faa3b85e6ea990302a77b603a893882626168783
OddinYa <oddinserj@gmail.com>
2022-06-06 23:50:07 +0300

    second commit

--- testTask/src/Port.java
+++ testTask/src/Port.java
@@ -57,25 +57,26 @@ public class Port implements StringOfArrayToArrayOfInteger, StringArrayToInteger
         return result;
     }
 
+    //Не работает...
     private ArrayList<ArrayList<Integer>> arrayIntegerArrayToAllPossibleOrderedPairs(ArrayList<ArrayList<Integer>>
                                                                                              toPossibleOrderedPairs) {
+
         ArrayList<ArrayList<Integer>> resutl = new ArrayList<>();
+
         if (toPossibleOrderedPairs.size() <= 1) {
             return toPossibleOrderedPairs;
         }
         ArrayList<Integer> temp = new ArrayList<>();
         ArrayList<Integer> first = toPossibleOrderedPairs.get(0);
         for (int i = 0; i < first.size(); i++) {
+            temp.add(first.get(i));
             for (int j = 1; j < toPossibleOrderedPairs.size(); j++) {
-                temp.add(first.get(i));
-                for (int k = i; k < toPossibleOrderedPairs.get(j).size() &&
-                        k == i; ) {
+                for (int k = i; k < toPossibleOrderedPairs.get(j).size();) {
                     temp.add(toPossibleOrderedPairs.get(j).get(k));
-                    k++;
+                    break;
                 }
                 resutl.add(arrayInteger(temp));
             }
-
         }
 
 
